484f411c00a88af126493e5e6f8e5c0541e88760
yperevera <y.perevera@gmail.com>
2017-11-09 18:59:21 +0200

    Tests for 'catmath.py' module was added.

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,15 +2,24 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    cat_age = 8
+    expected_hooman_age = 40
+    actual_hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert actual_hooman_age == expected_hooman_age
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    cat_age = 0.32
+    expected_hooman_age = 1.6
+    actual_hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert actual_hooman_age == expected_hooman_age
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    cat_age = 0
+    expected_hooman_age = 0
+    actual_hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert actual_hooman_age == expected_hooman_age
 
 
 # BONUS MATERIAL FOR STEP 2
